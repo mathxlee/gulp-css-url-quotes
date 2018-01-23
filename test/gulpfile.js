a64d28e68eab1ed1a9d04638599f9de181926330
mathxlee <math.lee@qq.com>
2018-01-23 16:05:31 +0800

    remove: remove test package

--- test/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var cssUrlQuotes = require('gulp-css-url-quotes');
-
-gulp.task('default', function() {
-    return gulp.src('./test.css')
-        .pipe(cssUrlQuotes())
-        .pipe(gulp.dest('build'));
-});
